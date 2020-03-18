1cc46e60f447d9348e44170afa025bf503014b8b
kevin.mansoldo <kevin.mansoldo@digitronica.it>
2020-03-18 17:12:58 +0100

    text editor, project refactor

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,9 +1,5 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic/';
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 
 @Component({
@@ -12,46 +8,63 @@ import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64u
   styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent implements OnInit, AfterViewInit {
-  constructor() { }
+  constructor() {
+  }
+
   public Editor = ClassicEditor;
-  public dataHtml  = '<p>Hello, world!</p>';
-  public editorConfig = {
-    plugins: [ Essentials, Paragraph, Bold, Italic, Base64UploadAdapter ],
-    toolbar: {
-      items: [
-        'bold',
-        'italic'
-      ]
-    },
-    // image: {
-    //   toolbar: [
-    //     'imageStyle:full',
-    //     'imageStyle:side',
-    //     '|',
-    //     'imageTextAlternative'
-    //   ]
-    // },
-    language: 'en',
-    data: this.dataHtml
-  };
+  public dataHtml = '<p>Hello, world!</p>';
+  public editorConfig = {};
 
   ngOnInit(): void {
+
   }
 
   ngAfterViewInit(): void {
-    ClassicEditor.create(document.querySelector('#editor'), this.editorConfig)
-      .then(newEditor => this.Editor = newEditor)
-      .catch( error => {
-        console.error( error );
-      });
   }
 
   onReady(e) {
-    console.log(e);
+    e.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+      return new Adapter(loader);
+    };
   }
 
   uploadData() {
-    console.log(this.Editor.getData());
+    console.log(this.dataHtml);
   }
 
+}
+
+class Adapter {
+  private reader: any;
+  private loader: any;
+
+  constructor( loader ) {
+    this.loader = loader;
+  }
+
+  upload() {
+    return new Promise( ( resolve, reject ) => {
+      const reader = this.reader = new window.FileReader();
+
+      reader.addEventListener( 'load', () => {
+        resolve( { default: reader.result } );
+      } );
+
+      reader.addEventListener( 'error', err => {
+        reject( err );
+      } );
+
+      reader.addEventListener( 'abort', () => {
+        reject();
+      } );
+
+      this.loader.file.then( file => {
+        reader.readAsDataURL( file );
+      } );
+    } );
+  }
+
+  abort() {
+    this.reader.abort();
+  }
 }
